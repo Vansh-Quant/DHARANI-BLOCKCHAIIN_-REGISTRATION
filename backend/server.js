9629686fd020cd0c11236fa858f622ad2ac45bc2
Vansh-Quant <vanshchoudhary2528@gmail.com>
2026-03-04 22:46:23 +0530

    UI design complete and ready for new laptop

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Forces Google's DNS
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
